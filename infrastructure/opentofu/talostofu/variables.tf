402b4db011a3bc109448be9a2f54f867871c4464
antblu <antblue2014@gmail.com>
2026-09-05 15:22:19 -0600

    increased vCPU of worker nodes

--- infrastructure/opentofu/talostofu/variables.tf
+++ infrastructure/opentofu/talostofu/variables.tf
@@ -138,7 +138,7 @@ variable "se350_worker_vm_memory" {
 variable "se350_worker_vm_cores" {
   type        = number
   description = "Number of vCPUs for worker VMs"
-  default     = 8
+  default     = 10
 }
 
 variable "se350_worker_vm_disk_size" {
