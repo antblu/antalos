f5df3d3404de362e30e76c3977a500f3e5f14b2a
antblu <antblue2014@gmail.com>
2026-09-14 06:09:16 -0600

    memory allocations

--- infrastructure/opentofu/lefttofu/variables.tf
+++ infrastructure/opentofu/lefttofu/variables.tf
@@ -54,13 +54,13 @@ variable "vm_cores" {
 variable "vm_memory" {
   type        = number
   description = "Dedicated VM memory in MiB"
-  default     = 8192
+  default     = 6144
 }
 
 variable "vm_ballooning_minimum" {
   type        = number
   description = "Minimum VM memory in MiB when ballooning"
-  default     = 6144
+  default     = 0
 }
 
 variable "vm_disk_size" {
